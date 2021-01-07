e568da48b794b1b3fe0354774de008fabf4ca9f1
james gill <jimmygill28@hotmail.com>
2021-01-07 13:42:29 +0000

    catch errors

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -6,27 +6,43 @@ const {
 } = require("../model/contacts.model");
 
 exports.getAllContacts = (req, res, next) => {
-  fetchAllContacts().then((contact) => {
-    res.json(contact);
-  });
+  fetchAllContacts()
+    .then((contact) => {
+      res.json(contact);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.postNewContacts = (req, res, next) => {
-  postContact(req.body).then((contact) => {
-    res.json(contact);
-  });
+  postContact(req.body)
+    .then((contact) => {
+      res.json(contact);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.patchContact = (req, res, next) => {
   const { id, first_name, last_name, phone, email } = req.body;
-  patchAContact(id, first_name, last_name, phone, email).then((contact) => {
-    res.json({ msg: "Contact Updated", contact });
-  });
+  patchAContact(id, first_name, last_name, phone, email)
+    .then((contact) => {
+      res.json({ msg: "Contact Updated", contact });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.deleteContact = (req, res, next) => {
   const { id } = req.body;
-  removeContact(id).then(() => {
-    res.sendStatus(204);
-  });
+  removeContact(id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
