930f6e5fc2c119eda99f71117ea2e40d58c049b7
james gill <jimmygill28@hotmail.com>
2021-02-12 14:16:44 +0000

    fix

--- listen.js
+++ listen.js
@@ -1,3 +1,9 @@
-const { PORT = 9090 } = process.env;
+// const { PORT = 9090 } = process.env;
 const app = require("./app");
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
