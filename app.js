48a7b3744ef943e5914231285dade03a2c5ea7c5
james gill <jimmygill28@hotmail.com>
2021-03-15 17:45:30 +0000

    added some error handling

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const apiRouter = require("./routes/api.router");
+const {
+  customErrorHandler,
+  handlePSQLErrors,
+  handle405Errors,
+  handleServerErrors,
+} = require("./error_handlers/index");
 
 app.use(cors());
 app.use(express.json());
@@ -11,5 +17,10 @@ app.use("/api", apiRouter);
 app.all("*", (req, res, next) => {
   next({ status: 404, msg: "Route not found" });
 });
+
+app.use(customErrorHandler);
+app.use(handlePSQLErrors);
+app.use(handle405Errors);
+app.use(handleServerErrors);
 
 module.exports = app;
--- routes/contacts.router.js
+++ routes/contacts.router.js
@@ -5,12 +5,14 @@ const {
   postNewContacts,
   deleteContact,
 } = require("../controllers/contacts.controller");
+const { handle405Errors } = require("../error_handlers/index");
 
 contactsRouter
   .route("/")
   .get(getAllContacts)
   .post(postNewContacts)
   .patch(patchContact)
-  .delete(deleteContact);
+  .delete(deleteContact)
+  .all(handle405Errors);
 
 module.exports = contactsRouter;
